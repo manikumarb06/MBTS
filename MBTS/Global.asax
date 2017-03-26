fb3a8973b1634a5dab0b5d41964e8274252a96cc
manikumarb06 <manikumarb06>
2017-03-26 16:58:08 -0400

    Created Project Initial Commit

---
+++ MBTS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MBTS.MvcApplication" Language="C#" %>
---
+++ MBTS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MBTS.Startup))]
+namespace MBTS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
